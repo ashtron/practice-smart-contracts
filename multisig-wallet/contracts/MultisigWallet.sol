4baf747009d5495a79a94c9f2bd738ac6aae0264
ashtron <ashton.playsted@protonmail.com>
2020-09-15 20:26:05 +0200

    Add solution draft (untested)

--- multisig-wallet/contracts/MultisigWallet.sol
+++ multisig-wallet/contracts/MultisigWallet.sol
@@ -3,30 +3,77 @@
 pragma solidity ^0.7.0;
 
 contract MultisigWallet {
-    address owner;
-    address[] signers;
+    address public owner;
+    uint public maxSigners;
+    uint public numSigners;
+    mapping(address => bool) public signers;
     TransferProposal[] transferProposals;
 
     struct TransferProposal {
-        address receiver;
+        uint id;
+        address payable receiver;
         uint amount;
+        uint yesVotes;
+        uint noVotes;
     }
 
-    modifier onlyOwner { _; }
-    modifier onlySigner { _; }
-    modifier spotAvailable { _; }
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
-    constructor() {}
+    modifier onlySigner {
+        require(signers[msg.sender]);
+        _;
+    }
+    modifier spotAvailable {
+        require(numSigners < maxSigners);
+        _;
+    }
 
-    function addSigner(address _newSigner) onlyOwner spotAvailable public {}
+    constructor() {
+        maxSigners = 3;
+        owner = msg.sender;
+        addSigner(owner);
+    }
+
+    // add check that signer hasn't already been added
+    function addSigner(address _newSigner) onlyOwner spotAvailable public {
+        signers[_newSigner] = true;
+        numSigners += 1;
+    }
 
     function deposit() onlySigner public payable {}
 
-    function proposeTransfer(address _receiver, uint _amount) onlySigner public {}
+    function proposeTransfer(address payable _receiver, uint _amount) onlySigner public {
+        transferProposals.push(TransferProposal({
+            id: transferProposals.length,
+            receiver: _receiver,
+            amount: _amount,
+            yesVotes: 0,
+            noVotes: 0
+        }));
+    }
 
-    function voteOnTransferProposal(uint _proposalId, bool _vote) onlySigner public {}
+    function voteOnTransferProposal(uint _proposalId, bool _vote) onlySigner public {
+        if (_vote) {
+            transferProposals[_proposalId].yesVotes += 1;
 
-    function executeTransfer(uint _proposalId) private {}
+            if (transferProposals[_proposalId].yesVotes >= 2) {
+                executeTransfer(_proposalId);
+            }
+        } else {
+            transferProposals[_proposalId].noVotes += 1;
+        }
+    }
 
-    fallback() external {}
+    function executeTransfer(uint _proposalId) private {
+        TransferProposal memory proposal = transferProposals[_proposalId];
+
+        proposal.receiver.transfer(proposal.amount);
+    }
+
+    fallback() external {
+        revert();
+    }
 }
